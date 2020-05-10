5302cc7730e4d9bb284a4242012e273a7f7ff898
saichandra datrika <datrikasaichandra@gmail.com>
2020-05-10 21:04:03 +0530

    first commit

---
+++ SampleCodeUploadtoGit/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleCodeUploadtoGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
